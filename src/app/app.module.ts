2f6d66d09c98d34fef2d5e91ba87d82017554864
MAllena <MAllena@rosariobus.com.ar>
2022-09-07 14:19:37 -0300

    pruebas angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FormSearchComponent } from './shared/components/form-search/form-search.component';
+import { HomeModule } from './components/pages/home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
